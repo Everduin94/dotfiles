ba7118654358522e0be9154cb91c0d7838da0f6c
Erik Verduin <everduin@cisco.com>
2022-09-02 06:58:29 -0500

    astro ls and svelte ls

---
+++ nvim/.config/nvim/lua/user/lsp/astro-ls.lua
@@ -0,0 +1,2 @@
+-- npm install -g @astrojs/language-server
+require'lspconfig'.astro.setup{}
---
+++ nvim/.config/nvim/lua/user/lsp/svelte-ls.lua
@@ -0,0 +1,4 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#svelte
+-- npm install -g svelte-language-server
+-- npm install --save-dev typescript-svelte-plugin
+require'lspconfig'.svelte.setup{}
